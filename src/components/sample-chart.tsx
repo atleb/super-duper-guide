4417768185009c0f0d591ec44b4039d4a8fff973
Atle Brandt <845667+atleb@users.noreply.github.com>
2024-07-16 23:25:46 +0200

    feat: add actual shacdn baseChart and use

---
+++ src/components/sample-chart.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Bar, BarChart } from "recharts";
+
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+
+import chartData from "./../data/cal";
+
+// todo: add to data/cal file?
+// todo: plain css vars for colors? vs config object?
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig;
+
+export default function Component() {
+  return (
+    <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+      <BarChart accessibilityLayer data={chartData}>
+        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+      </BarChart>
+    </ChartContainer>
+  );
+}
