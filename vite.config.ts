8ea28c90c0f0c1781186fa11b87ef74a2ee28316
Atle Brandt <845667+atleb@users.noreply.github.com>
2024-08-14 22:59:18 +0200

    fix: config for github pages assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
- 
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
+  base: "/super-duper-guide/",
   plugins: [react()],
-  resolve: { /* shacdn -> tailwind */
+  resolve: {
+    /* shacdn -> tailwind */
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
