f7fa3b329a0e370ce8e78a537d76008e9ce2f8d4
Atle Brandt <845667+atleb@users.noreply.github.com>
2025-07-16 23:35:03 +0200

    feat: tailwind 4, vite 7, remove postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/super-duper-guide/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     /* shacdn -> tailwind */
     alias: {
